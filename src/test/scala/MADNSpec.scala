c69e4aabb607905472f25d4f51c8a0c005e332a1
LeopoldSappler <leopold.sappler@gmail.com>
2022-04-08 08:13:21 +0200

    Project structure

--- src/test/scala/MADNSpec.scala
+++ src/test/scala/MADNSpec.scala
@@ -1,5 +1,5 @@
+import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.wordspec.AnyWordSpec
-import org.scalatest.matchers.should.Matchers._
 
 class MADNSpec extends AnyWordSpec{
   val madn = new MADN
